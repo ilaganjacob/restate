70f768c23295bd0e928b423c272f24edc525e628
Jacob Ilagan <jilagan5@myseneca.ca>
2025-01-15 18:22:06 -0500

    making tabs and properties

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,11 @@
 import {View, Text} from 'react-native'
 import React from 'react'
 
-const SignIn = () => {
+const Profile = () => {
     return (
         <View>
-            <Text>SignIn</Text>
+            <Text>Profile</Text>
         </View>
     )
 }
-export default SignIn
+export default Profile
